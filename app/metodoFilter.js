50b6138eaffa69ea4492e65d0e157b00165f5fe8
guisposito <guicarlosjoinhas@gmail.com>
2023-06-07 13:34:50 -0300

    Metodo filter

---
+++ app/metodoFilter.js
@@ -0,0 +1,21 @@
+const botoes = document.querySelectorAll('.btn');
+
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    console.table(livrosFiltrados);
+}
+
+
+//Procura apenas 1 elemento especifico
+// const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront');
+
+// btnFiltrarLivrosFront.addEventListener('click', filtrarLivros);
+
+// function filtrarLivros() {
+//     let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end' );
+//     console.table(livrosFiltrados);
+// }
